9e80d1db3d2d3261e7f5db36d2aa619fb98769b5
AlexRixten <rixten.09@gmail.com>
2022-12-12 23:15:59 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+/**
+ * @format
+ */
+
+import {AppRegistry} from 'react-native';
+import App from './App';
+import {name as appName} from './app.json';
+import {TabTwoScreen} from './src/components/TabTwoSreen';
+
+AppRegistry.registerComponent(appName, () => TabTwoScreen);
